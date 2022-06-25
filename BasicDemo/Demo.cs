f08023bf6099e097c5b68ed13c5da4e813fa3264
BertyBasset <reverendrythym@googlemail.com>
2022-06-25 15:56:55 +0100

    Add files via upload

---
+++ BasicDemo/Demo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Synth {
+
+    internal class Demo : WaveProvider32 {
+        private AsioOut? asioOut;
+
+        internal Demo() {
+            SetWaveFormat(44100, 2);    // Sample Rate, Channels
+            asioOut = new AsioOut();    // Audio Stream Input/Output protocol api
+            asioOut.Init(this);
+            asioOut.Play();
+        }
+
+        internal void Stop() {
+            asioOut.Stop();
+            asioOut.Dispose();
+            asioOut = null;
+        }
+
+
+        public override int Read(float[] buffer, int offset, int sampleCount) {
+            var r = new Random();
+            for (int n = 0; n < sampleCount; n++) {
+                var v = (float)((r.NextDouble() - .5) * .2);
+                buffer[n + offset] = v;
+            }
+            return sampleCount;
+        }
+    }
+}
---
+++ BasicDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿internal static class Program {
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main() {
+        var demo = new Synth.Demo();
+
+
+        // Produce white noise for 10 seconds
+        var startTime = DateTime.Now;
+        while ((DateTime.Now - startTime).TotalSeconds < 10) { } 
+        demo.Stop();
+        
+    }
+}
