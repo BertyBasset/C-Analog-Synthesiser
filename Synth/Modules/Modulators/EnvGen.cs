e95c22b14385f15594c873337ba1a24a7a1f5e30
BertyBasset <reverendrythym@googlemail.com>
2022-06-30 23:04:47 +0100

    Add files via upload

---
+++ Synth/Modules/Modulators/EnvGen.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synth.Modules.Modulators {
+    internal class EnvGen : iModulator {
+        public float Value {get; set;}
+    }
+}
---
+++ Synth/Modules/Modulators/ModWheel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synth.Modules.Modulators {
+    public class ModWheel : iModulator {
+        private float _value;
+        public float Value {
+            get { return _value; }
+            set { _value = value/127f; }
+        
+        }
+    }
+}
